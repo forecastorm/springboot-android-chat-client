9d65e8f20ef32aa99464823eccaf6bbbc5443d71
Fnil <jiangheyy@gmail.com>
2019-05-25 18:57:58 -0500

    start with random-stranger chat now

--- app/src/main/java/com/nil/client/config/InternetAddress.java
+++ app/src/main/java/com/nil/client/config/InternetAddress.java
@@ -1,6 +1,12 @@
 package com.nil.client.config;
 
 public class InternetAddress {
+
     public static final String webSocketAddress = "ws://ec2-13-59-82-93.us-east-2.compute.amazonaws.com:8080/my-ws/websocket";
+
+    //public static final String webSocketAddress = "ws://192.168.1.2:8080/my-ws/websocket";
+
     public static final String volleyUserPostAddress = "http://ec2-13-59-82-93.us-east-2.compute.amazonaws.com:8080/api/users/post";
+
+  //  public static final String volleyUserPostAddress = "http://192.168.1.2:8080/api/users/post";
 }
